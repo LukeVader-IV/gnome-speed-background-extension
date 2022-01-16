39c6fcb0d84082295f57b7295d42c2049f01344f
luke vader <cont-misc.xm2mj@aleeas.com>
2022-01-16 11:04:24 +0100

    fix accidentally broke extension on last version

--- extension.js
+++ extension.js
@@ -6,21 +6,21 @@ const background = imports.ui.background;
 const init_opacity = background.ANIMATION_OPACITY_STEP_INCREMENT 
 const init_wakeup = background.ANIMATION_MIN_WAKEUP_INTERVAL
 const ExtensionUtils = imports.misc.extensionUtils;
-const settings = null;
+// let settings = null;
 
 class Extension{
 	constructor(){
 	}
 	
 	enable(){
-		settings = ExtensionUtils.getSettings('org.gnome.shell.extensions.speedbackground');
+		// let settings = ExtensionUtils.getSettings('org.gnome.shell.extensions.speedbackground');
 		
 		background.ANIMATION_OPACITY_STEP_INCREMENT = OPACITY_STEP.get();
 		background.ANIMATION_MIN_WAKEUP_INTERVAL = WAKEUP_INTERVAL.get();
 	}
 	
 	disable(){
-		settings = null;
+		// let settings = null;
 
 		background.ANIMATION_OPACITY_STEP_INCREMENT = init_opacity;
 		background.ANIMATION_MIN_WAKEUP_INTERVAL = init_wakeup;
@@ -36,12 +36,12 @@ function init() {
 this.OPACITY_STEP = {
     key: 'opacity-step',
     get: function () {
-        return settings.get_double(this.key);
+        return ExtensionUtils.getSettings('org.gnome.shell.extensions.speedbackground').get_double(this.key);
     }
 };
 this.WAKEUP_INTERVAL = {
     key: 'wakeup-interval',
     get: function () {
-        return settings.get_double(this.key);
+        return ExtensionUtils.getSettings('org.gnome.shell.extensions.speedbackground').get_double(this.key);
     }
 };

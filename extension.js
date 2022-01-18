2ef58b420eda697acc6400b89e61959a79446a44
Luke Vader <cont-misc.xm2mj@aleeas.com>
2022-01-18 20:46:15 +0100

    use one instance of settings

--- extension.js
+++ extension.js
@@ -6,21 +6,21 @@ const background = imports.ui.background;
 const init_opacity = background.ANIMATION_OPACITY_STEP_INCREMENT 
 const init_wakeup = background.ANIMATION_MIN_WAKEUP_INTERVAL
 const ExtensionUtils = imports.misc.extensionUtils;
-// let settings = null;
+let settings = null;
 
 class Extension{
 	constructor(){
 	}
 	
 	enable(){
-		// let settings = ExtensionUtils.getSettings('org.gnome.shell.extensions.speedbackground');
+		settings = ExtensionUtils.getSettings('org.gnome.shell.extensions.speedbackground');
 		
 		background.ANIMATION_OPACITY_STEP_INCREMENT = OPACITY_STEP.get();
 		background.ANIMATION_MIN_WAKEUP_INTERVAL = WAKEUP_INTERVAL.get();
 	}
 	
 	disable(){
-		// let settings = null;
+		settings = null;
 
 		background.ANIMATION_OPACITY_STEP_INCREMENT = init_opacity;
 		background.ANIMATION_MIN_WAKEUP_INTERVAL = init_wakeup;
@@ -36,12 +36,12 @@ function init() {
 this.OPACITY_STEP = {
     key: 'opacity-step',
     get: function () {
-        return ExtensionUtils.getSettings('org.gnome.shell.extensions.speedbackground').get_double(this.key);
+        return settings.get_double(this.key);
     }
 };
 this.WAKEUP_INTERVAL = {
     key: 'wakeup-interval',
     get: function () {
-        return ExtensionUtils.getSettings('org.gnome.shell.extensions.speedbackground').get_double(this.key);
+        return settings.get_double(this.key);
     }
 };
